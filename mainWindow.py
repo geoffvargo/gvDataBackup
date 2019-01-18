7f433a114ae3305db248a6b3e0efb676efc5ec42
geoffvargo <geoff.vargo@stonybrook.edu>
2019-01-18 00:39:07 -0500

    code cleanup; extra comments added

--- mainWindow.py
+++ mainWindow.py
@@ -35,12 +35,11 @@ class MainWindowUI(QtWidgets.QMainWindow):
 
 		self.model_1 = QFileSystemModel()
 		self.model_1.setRootPath('')
-		# self.model_1.sel
 
 		self.model_2 = QFileSystemModel()
 		self.model_2.setRootPath('')
 
-		# link QML elements to Python variables
+		### link QML elements to Python variables ###
 		self.mainWin: QMainWindow = self.ui.findChild(QMainWindow, "MainWindow")
 		self.srcDirView: QTreeView = self.ui.findChild(QTreeView, "sourceDirView")
 		self.dstDirV: QTreeView = self.ui.findChild(QTreeView, "destDirView")
@@ -49,22 +48,38 @@ class MainWindowUI(QtWidgets.QMainWindow):
 		self.srcPathLNE: QLineEdit = self.ui.findChild(QLineEdit, "sourcePathLNE")
 		self.dstPathLNE: QLineEdit = self.ui.findChild(QLineEdit, "destPathLNE")
 
+		### set up file-system models ###
 		self.srcDirView.setModel(self.model_1)
 		self.srcFlistView.setModel(self.model_1)
-
-		self.srcDirView.clicked.connect(self.srcDirSelected)
-
 		self.dstDirV.setModel(self.model_2)
 		self.dstFlistView.setModel(self.model_2)
 
+		### selecting item(s) in treeview(s) return(s) pathname(s) ###
+		self.srcDirView.clicked.connect(self.srcDirSelected)
+		self.dstDirV.clicked.connect(self.dstDirSelected)
+
 	@pyqtSlot(QModelIndex)
 	def srcDirSelected(self, index):
+		''' Get selected path from srcDirView '''
 		file_path = self.model_1.index(index.row(), 0, index.parent())
 
 		filename = self.model_1.fileName(file_path)
 		pathname = self.model_1.filePath(file_path)
-		
+
 		print(pathname + filename)
+
+		return pathname
+
+	@pyqtSlot(QModelIndex)
+	def dstDirSelected(self, index):
+		''' Get selected path from dstDirView '''
+		currentRow = self.model_2.index(index.row(), 0, index.parent())
+
+		pathname = self.model_2.filePath(currentRow)
+
+		print(pathname)
+
+		return pathname
 
 	@pyqtSlot()
 	def srcDirViewClick(self):
